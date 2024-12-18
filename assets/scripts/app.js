07b59155ac76a6b1d93f3017f0d2a7e4552e6aba
Youil-Park <parkyouil98@gmail.com>
2024-12-18 17:27:30 +0900

    강한 공격 기능 추가하기

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;     // 플레이어의 최대 공격력
+const STRONG_ATTACK_VALUE = 17;   // 최대 강한 공격력
 const MONSTER_ATTACK_VALUE = 14;  // 몬스터의 최대 공격력
 
 let chosenMaxLife = 100;     // 몬스터의 최대 체력
@@ -7,8 +8,16 @@ let currentPlayerHealth = chosenMaxLife;  // 플레이어 체력 조정
 
 adjustHealthBars(chosenMaxLife);  // 최대 체력을 설정하는 체력 조정 함수
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
+// ATTACK, STRONG ATTACK 버튼 핸들러
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK') { // ATTACK 버튼
+    maxDamage = ATTACK_VALUE;
+  } else if(mode === 'STRONG_ATTACK'){  // STRONG ATTACK 버튼
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+
+  const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage; // 몬스터 체력 - 공격력
 
   // 몬스터가 플레이어 공격
@@ -25,4 +34,15 @@ function attackHandler() {
   }
 }
 
+// ATTACK
+function attackHandler() {
+  attackMonster('ATTACK');
+}
+
+// STRONG ATTACK
+function strongAttackHadler() {
+  attackMonster('STRONG_ATTACK');
+}
+
 attackBtn.addEventListener('click', attackHandler);  // 공격버튼
+strongAttackBtn.addEventListener('click', strongAttackHadler);  // 강한 공격 버튼
